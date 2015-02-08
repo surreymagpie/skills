09fca7d6f3523f442d48df6ced453a5e4028948a
Rob Curtis <surreymagpie@yahoo.co.uk>
2015-02-08 21:42:34 +0000

    Seed data for users and password validation

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,11 @@ class User < ActiveRecord::Base
 
   validates :first_name, :last_name, :email, presence: true
   validates :email, uniqueness: true
+  validates :password,  confirmation: true,
+                        length: { minimum: 6 },
+                        if: :password_required?
+
+  def password_required?
+    new_record? || !password.nil?
+  end
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,21 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+# Passwords for each are "secret" !!
+# Joe Bloggs is an admin
+users = [{ first_name: "Joe",  last_name: "Bloggs", email: "joe.bloggs@example.com", password: "secret", password_confirmation: "secret", admin: true },
+  { first_name: "Jane", last_name: "Smith",  email: "jane.smith@example.com", password: "secret", password_confirmation: "secret" },
+  { first_name: "Bill", last_name: "Brown",  email: "bill.brown@example.com", password: "secret", password_confirmation: "secret" }]
+
+users.each do |u|
+  user = User.create(u)
+  if user.errors.blank?
+    puts "***User #{user.first_name} #{user.last_name} created ***"
+  else
+    puts "User failed to create due to below reasons:"
+    user.errors.each do |x, y|
+       puts"#{x} #{y}" # x will be the field name and y will be the error on it
+    end
+  end
+end
